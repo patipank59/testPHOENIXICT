eeeefc7505d2455c16e03c7c873454b9a0c1986f
exeptionz <suchat.m.utcc@gmail.com>
2020-05-25 05:23:59 +0700

    Init

---
+++ test1.py
@@ -0,0 +1,20 @@
+# จัดลำดับของ player ตาม score และใส่ rank ให้กับตามลำดับแรงจาก score มากสุด
+
+player = [
+    {'Name':'A', 'score': 50},
+    {'Name':'B', 'score': 60},
+    {'Name':'C', 'score': 85},
+    {'Name':'D', 'score': 80},
+    {'Name':'E', 'score': 90}
+]
+
+rank = ['Gold', 'Silver', 'Bronze']
+
+# ผลลัพธ์
+# [
+#     {'Name': 'E', 'score': 90, 'rank': 'Gold'}, 
+#     {'Name': 'C', 'score': 85, 'rank': 'Silver'}, 
+#     {'Name': 'D', 'score': 80, 'rank': 'Bronze'}, 
+#     {'Name': 'B', 'score': 60, 'rank': '-'}, 
+#     {'Name': 'A', 'score': 50, 'rank': '-'}
+# ]
---
+++ test2.py
@@ -0,0 +1,16 @@
+# สร้าง function หาชื่อของ key ใน obj ด้านล่างที่มี value เป็น 'value5'
+
+obj = { 
+    "key1": 
+    {
+        "key2" : "value1",
+        "key3" : "value2",
+        "key4" : 
+        {
+            "key5": "value5"
+        }
+    }
+}
+
+# result
+# 'key5'
